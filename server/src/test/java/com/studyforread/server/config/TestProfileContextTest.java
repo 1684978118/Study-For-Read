127035f6e32cf394190d52dca215cbc5efcdb1ac
Yuyan <yuyan@example.com>
2026-05-06 22:13:36 +0900

    chore: configure postgres and flyway profiles

---
+++ server/src/test/java/com/studyforread/server/config/TestProfileContextTest.java
@@ -0,0 +1,14 @@
+package com.studyforread.server.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class TestProfileContextTest {
+
+    @Test
+    void startsWithTestProfile() {
+    }
+}
